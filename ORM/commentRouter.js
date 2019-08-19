f70f480ed7f4c728d0f250b6fdb71169d94afbf4
Mqspx800 <applefor1988@gmail.com>
2019-08-19 22:50:33 +0200

    commentrouter added

--- ORM/commentRouter.js
+++ ORM/commentRouter.js
@@ -1,3 +1,17 @@
-const { Event, Ticket } = require('./model')
+const { Comment } = require('./model')
 const { Router } = require('express')
 const router = new Router()
+
+router.post('/comment', async (req, res) => {
+  const { text, userId, ticketId } = req.body
+  console.log(req.body)
+  if (!text || text === '') res.status(422).send('not allow to post empty comment')
+  if (userId) {
+    const comment = await Comment.create({text, userId, ticketId})
+    res.json({ comment })
+  } else {
+    res.status(422).send('Please login before you try to comment')
+  }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const userRouter = require('./ORM/userRouter')
 const eventRouter = require('./ORM/eventRouter')
 const ticketRouter = require('./ORM/ticketRouter')
+const commentRouter = require('./ORM/commentRouter')
 const sse = require('json-sse')
 
 const port = process.env.PORT||5000
@@ -17,6 +18,7 @@ app.use(jsonParser)
 app.use(userRouter)
 app.use(eventRouter)
 app.use(ticketRouter)
+app.use(commentRouter)
 
 
 app.listen(port,()=>console.log(`server listening on port ${port}`))
